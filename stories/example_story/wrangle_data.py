a7a1dfd64cf80a017f35b19e2da6e3e211533dee
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2023-06-01 18:52:49 -0500

    Update wrangle_data.py

--- stories/example_story/wrangle_data.py
+++ stories/example_story/wrangle_data.py
@@ -1,1 +1,15 @@
 import pandas as pd
+from download_data import load_data_into_dataframe  # Assuming the previous script is named download_data.py
+
+def wrangle_data(df):
+    # Perform data wrangling operations here
+    # For example, let's drop any rows with missing values and rename a column
+    df = df.dropna()
+    df = df.rename(columns={"old_column_name": "new_column_name"})
+    return df
+
+if __name__ == "__main__":
+    file = "derived_data.csv"  # Replace with your file name
+    df = load_data_into_dataframe(file)
+    df = wrangle_data(df)
+    print(df)

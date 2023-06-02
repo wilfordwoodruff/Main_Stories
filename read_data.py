1b1c4c109d2ed83ad972226b91890fcad53f2c23
TylerEnglish <77080632+TylerEnglish@users.noreply.github.com>
2023-06-01 19:10:54 -0500

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -1,24 +1,10 @@
-import requests
 import pandas as pd
 
-def download_file_from_github(user, repo, file):
-    url = f"https://raw.githubusercontent.com/{user}/{repo}/main/{file}"
-    response = requests.get(url)
-    response.raise_for_status()  # Ensure we got a successful response
-
-    with open(file, 'wb') as f:
-        f.write(response.content)
-
-    print(f"Downloaded {file} from {user}/{repo}")
-
-def load_data_into_dataframe(file):
-    df = pd.read_csv(file)
+def read_data_from_url(url):
+    df = pd.read_csv(url)
     return df
 
 if __name__ == "__main__":
-    user = "tylerenglish"  # Replace with your GitHub username
-    repo = "Main-Data"  # Replace with your repository name
-    file = "derived_data.csv"  # Replace with your file name
-
-    download_file_from_github(user, repo, file)
-    df = load_data_into_dataframe(file)
+    url = "https://raw.githubusercontent.com/wilfordwoodruff/Main-Data/main/data/derived/derived_data.csv"
+    df = read_data_from_url(url)
+    print(df)
